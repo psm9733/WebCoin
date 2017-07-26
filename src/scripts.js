59a4524b8db93e233e8292f7cf841c29a13cc5f4
SangminPark <psm9733@naver.com>
2017-07-26 18:01:01 +0900

    add korbit

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,13 @@
+poloniex = "https://poloniex.com";
+bithumb = "https://www.bithumb.com";
+coinone = "https://coinone.co.kr";
+okcoin = "https://www.okcoin.com";
+bitflyer = "https://bitflyer.jp/";
+bitfinex = "https://www.bitfinex.com/";
+korbit = "www.korbit.co.kr/";
+huobi = "https://www.huobi.com";
+gimini = "https://gemini.com";
+
 function LogoEvent(){
 	$('#homepage_icon').click(function(){
 		location.href = "./main.html";
@@ -32,63 +42,64 @@ function currentSlide(n) {
 
 function ExchangeEvent(){
 	$('#linkone').click(function(){
-		location.href = "https://poloniex.com";
+		location.href = poloniex;
 	});
 	$('#linktwo').click(function(){
-		location.href = "https://www.bithumb.com";
+		location.href = bithumb;
 	});
 	$('#linkthree').click(function(){
-		location.href = "https://coinone.co.kr";
+		location.href = coinone;
 	});
 	$('#linkfour').click(function(){
-		location.href = "https://www.okcoin.com";
+		location.href = okcoin;
 	});
 	$('#linkfive').click(function(){
-		location.href = "https://bitflyer.jp/";
+		location.href = bitflyer;	
 	});
 	$('#linksix').click(function(){
-		location.href = "https://www.bitfinex.com/";
+		location.href = bitfinex;
 	});
 	$('#linkseven').click(function(){
-		location.href = "https://www.gdax.com";
+		location.href = gdax;
 	});
 	$('#linkeight').click(function(){
-		location.href = "www.korbit.co.kr/";
+		location.href = korbit;
 	});
 	$('#linknine').click(function(){
-		location.href = "https://www.huobi.com";
+		location.href = huobi;
 	});
 	$('#linkten').click(function(){
-		location.href = "https://gemini.com";
+		location.href = gemini;
 	});
 }
 
-function DashboardEvent(delay){
-	var delay = delay;
+function DashboardEvent(delay1, delay2){
+	var delay1 = delay1;
+	var	delay2 = delay2;
 	var Poloniex_BTC = Poloniex_ETH = Poloniex_XRP = Poloniex_ETC = Poloniex_LTC = Poloniex_DASH = 0.0;
-	var Bithumb_BTC = Bithumb_eth = Bithumb_xrp = Bithumb_etc = Bithumb_LTC = Bithumb_DASH = 0.0;
-	var coinone_btc = coinone_eth = coinone_xrp = coinone_etc = coinone_LTC = coinone_DASH = 0.0;
-	var OKCoin_BTC = OKCoin_eth = OKCoin_xrp = OKCoin_etc = OKCoin_LTC = OKCoin_DASH = 0.0;
-	var bitFlyer_BTC = bitFlyer_eth = bitFlyer_xrp = bitFlyer_etc = bitFlyer_LTC = bitFlyer_DASH = 0.0;
-	var Bitfinex_BTC = Bitfinex_eth = Bitfinex_xrp = Bitfinex_etc = Bitfinex_LTC = Bitfinex_DASH = 0.0;
-	var GDAX_BTC = GDAX_eth = GDAX_xrp = GDAX_etc = GDAX_LTC = GDAX_DASH = GDAX_VOULME = 0.0;
-	var Korbit_BTC = Korbit_eth = Korbit_xrp = Korbit_etc = Korbit_LTC = Korbit_DASH =0.0;
-	var Huobi_BTC = Huobi_eth = Huobi_xrp = Huobi_etc = Huobi_LTC = Huobi_DASH = 0.0;
-	var Gemini_BTC = Gemini_eth = Gemini_xrp = Gemini_etc = Gemini_LTC = Gemini_DASH =0.0;
-	var loopTimer = window.setInterval(function(){
-		// Poloniex_BTC = Coinone_getprice("Poloniex_BTC", 'https://api.coinone.co.kr/ticker?currency=btc');
-		// Poloniex_ETH = Coinone_getprice("Poloniex_ETH", 'https://api.coinone.co.kr/ticker?currency=eth');
-		// Poloniex_XRP = Coinone_getprice("Poloniex_XRP", 'https://api.coinone.co.kr/ticker?currency=xrp');
-		// Poloniex_ETC = Coinone_getprice("Poloniex_ETC", 'https://api.coinone.co.kr/ticker?currency=etc');
-		// Poloniex_LTC = Coinone_getprice("Poloniex_LTC");
-		// Poloniex_DASH = Coinone_getprice("Poloniex_DASH");
+	var Bithumb_BTC = Bithumb_ETH = Bithumb_xrp = Bithumb_ETC = Bithumb_LTC = Bithumb_DASH = 0.0;
+	var coinone_BTC = coinone_ETH = coinone_xrp = coinone_ETC = coinone_LTC = coinone_DASH = 0.0;
+	var OKCoin_BTC = OKCoin_ETH = OKCoin_XRP = OKCoin_ETC = OKCoin_LTC = OKCoin_DASH = 0.0;
+	var bitFlyer_BTC = bitFlyer_ETH = bitFlyer_XRP = bitFlyer_ETC = bitFlyer_LTC = bitFlyer_DASH = 0.0;
+	var Bitfinex_BTC = Bitfinex_ETH = Bitfinex_XRP = Bitfinex_ETC = Bitfinex_LTC = Bitfinex_DASH = 0.0;
+	var GDAX_BTC = GDAX_ETH = GDAX_XRP = GDAX_ETC = GDAX_LTC = GDAX_DASH = GDAX_VOULME = 0.0;
+	var Korbit_BTC = Korbit_ETH = Korbit_XRP = Korbit_ETC = Korbit_LTC = Korbit_DASH =0.0;
+	var Huobi_BTC = Huobi_ETH = Huobi_XRP = Huobi_ETC = Huobi_LTC = Huobi_DASH = 0.0;
+	var Gemini_BTC = Gemini_ETH = Gemini_XRP = Gemini_ETC = Gemini_LTC = Gemini_DASH =0.0;
+	var loopTimer1 = window.setInterval(function(){
+		Poloniex_BTC = Poloniex_getprice("Poloniex_BTC", 'https://poloniex.com/public?command=returnTicker', 'USDT_BTC');
+		Poloniex_ETH = Poloniex_getprice("Poloniex_ETH", 'https://poloniex.com/public?command=returnTicker', 'USDT_ETH');
+		Poloniex_XRP = Poloniex_getprice("Poloniex_XRP", 'https://poloniex.com/public?command=returnTicker', 'USDT_XRP');
+		Poloniex_ETC = Poloniex_getprice("Poloniex_ETC", 'https://poloniex.com/public?command=returnTicker', 'USDT_ETC');
+		Poloniex_LTC = Poloniex_getprice("Poloniex_LTC", 'https://poloniex.com/public?command=returnTicker', 'USDT_LTC');
+		Poloniex_DASH = Poloniex_getprice("Poloniex_DASH", 'https://poloniex.com/public?command=returnTicker', 'USDT_DASH');
 	
-		Bithumb_BTC = bithumb_getprice("Bithumb_BTC", 'https://api.bithumb.com/public/ticker?currency=btc');
-		Bithumb_ETH = bithumb_getprice("Bithumb_ETH", 'https://api.bithumb.com/public/ticker?currency=eth');
-		Bithumb_XRP = bithumb_getprice("Bithumb_XRP", 'https://api.bithumb.com/public/ticker?currency=xrp');
-		Bithumb_ETC = bithumb_getprice("Bithumb_ETC", 'https://api.bithumb.com/public/ticker?currency=etc');
-		Bithumb_LTC = bithumb_getprice("Bithumb_LTC", 'https://api.bithumb.com/public/ticker?currency=ltc');
-		Bithumb_DASH = bithumb_getprice("Bithumb_DASH", 'https://api.bithumb.com/public/ticker?currency=dash');
+		// Bithumb_BTC = bithumb_getprice("Bithumb_BTC", 'https://api.bithumb.com/public/ticker?currency=btc');
+		// Bithumb_ETH = bithumb_getprice("Bithumb_ETH", 'https://api.bithumb.com/public/ticker?currency=eth');
+		// Bithumb_XRP = bithumb_getprice("Bithumb_XRP", 'https://api.bithumb.com/public/ticker?currency=xrp');
+		// Bithumb_ETC = bithumb_getprice("Bithumb_ETC", 'https://api.bithumb.com/public/ticker?currency=etc');
+		// Bithumb_LTC = bithumb_getprice("Bithumb_LTC", 'https://api.bithumb.com/public/ticker?currency=ltc');
+		// Bithumb_DASH = bithumb_getprice("Bithumb_DASH", 'https://api.bithumb.com/public/ticker?currency=dash');
 		
 		Coinone_BTC = Coinone_getprice("Coinone_BTC", 'https://api.coinone.co.kr/ticker?currency=btc');
 		Coinone_ETH = Coinone_getprice("Coinone_ETH", 'https://api.coinone.co.kr/ticker?currency=eth');
@@ -96,84 +107,170 @@ function DashboardEvent(delay){
 		Coinone_ETC = Coinone_getprice("Coinone_ETC", 'https://api.coinone.co.kr/ticker?currency=etc');
 		Coinone_LTC = Coinone_getprice("Coinone_LTC");
 		Coinone_DASH = Coinone_getprice("Coinone_DASH");
-	}, delay);
+	}, delay1);
+	var loopTimer2 = window.setInterval(function(){
+		Korbit_BTC = Korbit_getprice("Korbit_BTC", 'https://api.korbit.co.kr/v1/ticker?currency_pair=btc_krw');
+		Korbit_ETH = Korbit_getprice("Korbit_ETH", 'https://api.korbit.co.kr/v1/ticker?currency_pair=eth_krw');
+		Korbit_XRP = Korbit_getprice("Korbit_XRP", 'https://api.korbit.co.kr/v1/ticker?currency_pair=xrp_krw');
+		Korbit_ETC = Korbit_getprice("Korbit_ETC", 'https://api.korbit.co.kr/v1/ticker?currency_pair=etc_krw');
+		Korbit_LTC = Korbit_getprice("Korbit_LTC");
+		Korbit_DASH = Korbit_getprice("Korbit_DASH");
+	}, delay2);
 }
 
-function setDashboardEvent(id, value){
+function setDashboardEvent_SC_status_Check(id){
+	$("#"+id).html("Server Connection Fail!");
+}
+
+function setDashboardEvent_KRW_price(id, value){
 	if(value == '' || value == null || value == undefined || value == 0 || value == NaN){
 		$("#"+id).html("X");
 		return;
 	}
-	
-	$("#"+id).html(numberWithCommas(value) + "KRW");
+	$("#"+id).html(numberWithCommas(value) + "원");
 }
 
-function bithumb_getprice(id, api){
+function setDashboardEvent_USDT_price(id, value){
+	if(value == '' || value == null || value == undefined || value == 0 || value == NaN){
+		$("#"+id).html("X");
+		return;
+	}
+	$("#"+id).html(numberWithCommas("$" + value.toFixed(3)));
+}
+
+function setDashboardEvent_str(id, value){
+	if(value == '' || value == null || value == undefined || value == 0 || value == NaN){
+		$("#"+id).html("X");
+		return;
+	}
+	$("#"+id).html("<b>환율 = " + value + "KRW</b>");
+}
+
+function Poloniex_getprice(id, url, cointype){
 	var id = id;
-	var api = api;
-	var price;
-	if(api == '' || api == null || api == undefined || api == 0 || api == NaN){
-		setDashboardEvent(id, price);
+	var url = url;
+	var price = 0.0;
+	if(url == '' || url == null || url == undefined || url == 0 || url == NaN){
+		setDashboardEvent_price(id, price);
 		return 0;
 	}
-
 	$.ajax({
-		url:api,
+		origin:"https://poloniex.com/",
+		url:url,
 		dataType:'json',
+		type: 'GET',
 		success:function(data){
-			price = data["closing_price"];
-			setDashboardEvent(id, price, type);
+			price = parseFloat(data[cointype]["last"]);
+			setDashboardEvent_USDT_price(id, price);
+		},
+		error: function(xhr) {
+			setDashboardEvent_SC_status_Check(id);
+			console.log('api실패 - ', xhr);
 		}
 	});
 	return price;
 }
 
-function Coinone_getprice(id, api){
+function bithumb_getprice(id, url){
 	var id = id;
-	var api = api;
-	var price;
-	var type = 'V';
-	if(api == '' || api == null || api == undefined || api == 0 || api == NaN){
-		setDashboardEvent(id, price);
+	var url = url;
+	var price = 0.0;
+	if(url == '' || url == null || url == undefined || url == 0 || url == NaN){
+		setDashboardEvent_price(id, price);
 		return 0;
 	}
 	$.ajax({
-		url:api,
+		crossOrigin: true,
+		origin:"https://api.bithumb.com/",
+		url:url,
 		dataType:'json',
+		type: 'GET',
+		success:function(data){
+			price = data["data"]["closing_price"];
+			setDashboardEvent_KRW_price(id, price);
+			return price;
+		},
+		error: function(xhr) {
+			setDashboardEvent_SC_status_Check(id);
+			console.log('api실패 - ', xhr);
+		}
+	});
+}
+
+function Coinone_getprice(id, url){
+	var id = id;
+	var url = url;
+	var price = 0.0;
+	if(url == '' || url == null || url == undefined || url == 0 || url == NaN){
+		setDashboardEvent_KRW_price(id, price);
+		return 0;
+	}
+	$.ajax({
+		origin:"https://api.coinone.co.kr/",
+		url:url,
+		dataType:'json',
+		type: 'GET',
 		success:function(data){
 			price = data["last"];
-			setDashboardEvent(id, price, type);		
+			setDashboardEvent_KRW_price(id, price);	
+		},
+		error: function(xhr) {
+			setDashboardEvent_SC_status_Check(id);
+			console.log('api실패 - ', xhr);
+			return price;
 		}
 	});
-	return price;
+}
+
+function Korbit_getprice(id, url){
+	var id = id;
+	var url = url;
+	var price = 0.0;
+	if(url == '' || url == null || url == undefined || url == 0 || url == NaN){
+		setDashboardEvent_KRW_price(id, price);
+		return 0;
+	}
+	$.ajax({
+		origin:"https://apidocs.korbit.co.kr/",
+		crossOrigin: true,
+		url:url,
+		dataType:'json',
+		type: 'GET',
+		success:function(data){
+			price = data["last"];
+			setDashboardEvent_KRW_price(id, price);	
+		},
+		error: function(xhr) {
+			setDashboardEvent_SC_status_Check(id);
+			console.log('api실패 - ', xhr);
+			return price;
+		}
+	});
 }
 
 function numberWithCommas(x) {
     return x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-function comma(num){
-    var len, point, str; 
-       
-    num = num + ""; 
-    point = num.length % 3 ;
-    len = num.length; 
-   
-    str = num.substring(0, point); 
-    while (point < len) { 
-        if (str != "") str += ","; 
-        str += num.substring(point, point + 3); 
-        point += 3; 
-    } 
-     
-    return str;
+// function comma(num){
+    // var len, point, str; 
+    // num = num + ""; 
+    // point = num.length % 3 ;
+    // len = num.length; 
+    // str = num.substring(0, point); 
+    // while (point < len) { 
+        // if (str != "") str += ","; 
+        // str += num.substring(point, point + 3); 
+        // point += 3; 
+    // } 
+    // return str;
  
-}
+// }
 
 //main
 $(document).ready(function(){
 	LogoEvent();
 	MenuEvent();
 	ExchangeEvent();
-	DashboardEvent(1000);		//1초
+	DashboardEvent(1000, 4000);		//1초
 });
